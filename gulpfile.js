759060b02b9a6daf95de6f6465fe4a4262ad93ac
Diego Costa <diego.machado.d.costa@gmail.com>
2019-08-14 11:09:36 -0300

    initial

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const gulp = require('gulp');
+const sass = require("gulp-sass");
+const html = require('gulp-htmlmin');
+
+sass.compiler = require('node-sass');
+
+// * Minify *
+
+gulp.task('sass', () => {
+    return gulp.src('./src/styles/sass/master.scss')
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./src/styles/css'));
+});
+
+gulp.task('html', () => {
+    return gulp.src('./src/views/*.html')
+        .pipe(html({
+            collapseWhitespace: true
+        }))
+        .pipe(gulp.dest('./dist'));
+});
+
+// * Default is a Observable :)*
+
+gulp.task('default', () => {
+    gulp.watch('./src/views/*.html', gulp.series('html'));
+    gulp.watch('./src/styles/sass/master.scss', gulp.series('sass'));
+});
