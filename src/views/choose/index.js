fe255af56094891f0d43bc41ddd1a862e8cd78af
Diego Costa <diego.machado.d.costa@gmail.com>
2019-10-02 10:09:52 -0300

    last

---
+++ src/views/choose/index.js
@@ -0,0 +1,12 @@
+
+const setHeight = () => {
+    const page = document.querySelector('.p-choose');
+    
+    page.style.height = `${window.innerHeight}px`;
+}
+
+document.body.onresize = () => {
+    setHeight();
+}
+
+setHeight()
